e9ee826184dedaec7b6fd90c050198a32c4e81ca
ghsr <ghsr92@yandex.ru>
2016-06-02 22:35:19 +0600

    i9105p: Omnify

--- full_i9105p.mk
+++ full_i9105p.mk
@@ -1,5 +1,5 @@
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9105p/i9105p.mk)
--- omni_i9105p.mk
+++ omni_i9105p.mk
@@ -1,14 +1,17 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit Omni GSM telephony parts
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit Omni product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+#$(call inherit-product, vendor/omni/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9105p/full_i9105p.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_i9105p
+PRODUCT_NAME := omni_i9105p
 
 # Set build fingerprint / ID / Prduct Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
